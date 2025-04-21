488e5596a33e35dd14253b649679d2bbc08d210c
Yaroslav <6i4bbb@gmail.com>
2025-04-21 21:31:49 +0300

    Implement a Google code style

--- src/main/java/ua/com/javarush/gnew/entity/Contact.java
+++ src/main/java/ua/com/javarush/gnew/entity/Contact.java
@@ -12,62 +12,58 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-@NamedQueries({
-        @NamedQuery(
-                name = "Contact.findByName",
-                query = "from Contact where name= :name"
-        )
-})
+@NamedQueries({@NamedQuery(name = "Contact.findByName", query = "from Contact where name= :name")})
 @Getter
 @Setter
 @NoArgsConstructor
 @Entity
 @Table(name = "contact")
 public class Contact {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Expose
-    private int id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Expose
+  private int id;
 
-    @Column(name = "name")
-    @Expose
-    private String name;
+  @Column(name = "name")
+  @Expose
+  private String name;
 
-    // Consider switching to LAZY loading if appropriate.
-    @OneToMany(mappedBy = "contact", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @Expose
-    private List<Email> emails = new ArrayList<>();
+  // Consider switching to LAZY loading if appropriate.
+  @OneToMany(mappedBy = "contact", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+  @Expose
+  private List<Email> emails = new ArrayList<>();
 
-    @OneToMany(mappedBy = "contact", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @Expose
-    private List<Phone> phones = new ArrayList<>();
+  @OneToMany(mappedBy = "contact", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+  @Expose
+  private List<Phone> phones = new ArrayList<>();
 
-    @OneToMany(mappedBy = "contact", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @Expose
-    private List<SocialNetwork> networks = new ArrayList<>();
+  @OneToMany(mappedBy = "contact", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+  @Expose
+  private List<SocialNetwork> networks = new ArrayList<>();
 
-    @CreationTimestamp
-    @Temporal(TemporalType.TIMESTAMP)
-    @Column(name = "create_date")
-    private Date createDate;
+  @CreationTimestamp
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(name = "create_date")
+  private Date createDate;
 
-    @UpdateTimestamp
-    @Temporal(TemporalType.TIMESTAMP)
-    @Column(name = "modify_date")
-    private Date modifyDate;
+  @UpdateTimestamp
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(name = "modify_date")
+  private Date modifyDate;
 
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
+  @Override
+  public boolean equals(Object o) {
+    if (o == null || getClass() != o.getClass()) return false;
 
-        Contact contact = (Contact) o;
-        return id == contact.id && name.equals(contact.name);
-    }
+    Contact contact = (Contact) o;
+    return id == contact.id && name.equals(contact.name);
+  }
 
-    @Override
-    public int hashCode() {
-        int result = id;
-        result = 31 * result + name.hashCode();
-        return result;
-    }
+  @Override
+  public int hashCode() {
+    int result = id;
+
+    result = 31 * result + name.hashCode();
+    return result;
+  }
 }
