d4f21b46af17dff580b303f1cb71ee66a211d8a4
Yaroslav <6i4bbb@gmail.com>
2025-05-21 18:33:02 +0300

    Long to Integer

--- src/main/java/ua/com/javarush/gnew/contactm/repository/ContactRepository.java
+++ src/main/java/ua/com/javarush/gnew/contactm/repository/ContactRepository.java
@@ -3,6 +3,6 @@ package ua.com.javarush.gnew.contactm.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ua.com.javarush.gnew.contactm.entity.Contact;
 
-public interface ContactRepository extends JpaRepository<Contact, Integer> {
+public interface ContactRepository extends JpaRepository<Contact, Long> {
 
 }
